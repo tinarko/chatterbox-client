0a3609bdb23e6da25f44d7604c490e78e8c909ad
tinarko <tinarchan@gmail.com>
2017-02-17 19:29:59 -0800

    Pass all tests up to chatroom behavior

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,98 @@
-// YOUR CODE HERE:
+var app = {};
+
+app.init = function () {
+
+};
+app.server = 'http://parse.sfm6.hackreactor.com/';
+
+app.send = function (message) {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data);
+    },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+
+};
+
+app.fetch = function () {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'GET',
+    data: JSON.stringify(),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data);
+    },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive messages', data);
+    }
+  });
+
+};
+
+app.clearMessages = function () {
+  $('#chats').empty();
+};
+
+app.renderMessage = function (message) {
+  var $chats = $('#chats');
+  // var $donkey = $('<p></p>');
+  // $donkey.addClass('username message');
+  // $donkey.text(message.text);
+  // $chats.append($donkey);
+  var text = message.text;
+
+  $chats.append('<p>' + message.text + '</p>');
+  console.log($chats);
+};
+
+app.renderRoom = function (room) {
+  var $roomSelect = $('#roomSelect');
+  $roomSelect.append('<option value=' + room + '>' + room + '</option>');
+};
+
+app.handleUserNameClick = function () {
+  // if clicked, traverse dom to find clicked class
+  $('#main').find('.username').trigger('click');
+  // add friend to clicked class
+};
+
+
+
+
+
+
+
+/*
+
+<div id="chats">
+  <div class="username message">
+    <p> Never underestimate the power of the Schwartz! </p>
+    <p clicked="false"> Mel brooks </p>
+  </div>
+</div>
+
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
